87818f4fa822507c2a035307863f006eb6c4b55f
ahhzaky <siahmadzaky@gmail.com>
2021-07-15 11:39:58 +0700

    :gear: write packagejson

---
+++ src/configs/config.js
@@ -0,0 +1,33 @@
+const inquirer = require("inquirer");
+const path = require("path");
+
+const baseConfig = {
+  version: "1.0.1",
+  description: "",
+  main: "index.js",
+  scripts: {
+    start: "nodemon server.js",
+    test: 'echo "Error: no test specified" && exit 1',
+  },
+  keywords: [],
+  author: "",
+  license: "ISC",
+};
+
+async function nodePackage(config) {
+  const answers = await inquirer.prompt([
+    {
+      type: "text",
+      name: "descriptionAPI",
+      message: "Description of the api project you want to make?",
+      default: "Api standard",
+    },
+  ]);
+  baseConfig.description = answers.descriptionAPI;
+  return {
+    ...config,
+    ...baseConfig,
+  };
+}
+
+module.exports = nodePackage;
--- src/index.js
+++ src/index.js
@@ -2,35 +2,57 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 
-const existingConfig = fs.existsSync("kage.json");
+const nodePackage = require("./configs/config");
+const packagePath = path.join(process.cwd(), "kage.json");
+const existingConfig = fs.existsSync(packagePath);
 
 console.log(process.cwd());
 
-function buildConfig() {
-  inquirer
-    .prompt([
-      {
-        type: "text",
-        name: "name",
-        message: "What is the name of the project api?",
-        default: path.basename(process.cwd()),
-      },
-      {
-        type: "list",
-        name: "typeAPI",
-        message: "Do you want the default endpoint?",
-        choices: ["only-express", "standard-endpoint"],
-      },
-      {
-        type: "list",
-        name: "typeDB",
-        message: "What database will you use?",
-        choices: ["MongoDB", "Mysql"],
-      },
-    ])
-    .then((answers) => {
-      console.log(answers);
-    });
+async function buildConfig() {
+  const answers = await inquirer.prompt([
+    {
+      type: "text",
+      name: "name",
+      message: "What is the name of the project api?",
+      default: path.basename(process.cwd()),
+    },
+
+    {
+      type: "list",
+      name: "typeAPI",
+      message: "Do you want the default endpoint?",
+      choices: ["only-express", "standard-endpoint"],
+    },
+    {
+      type: "list",
+      name: "typeDB",
+      message: "What database will you use?",
+      choices: ["MongoDB", "Mysql"],
+    },
+  ]);
+  let config = {
+    name: answers.name,
+  };
+  // .then((answers) => {
+  //   console.log(answers);
+  // });
+  config.name = answers.name;
+  console.log(answers);
+  switch (answers.typeDB) {
+    case "MongoDB":
+      config = await nodePackage(config);
+      console.log(config);
+      break;
+
+    //   case "Mysql":
+    //     console.log("This Mysql");
+    //     break;
+    default:
+      break;
+  }
+  fs.writeFileSync(packagePath, JSON.stringify(config, null, 2), "utf8");
+  console.log("All Done 👍👍");
+  process.exit(0);
 }
 
 // for check file.js
